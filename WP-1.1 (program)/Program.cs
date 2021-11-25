95017281e8cb8ee78b6d9d0cc6b7e94c133e9803
Matthew A. Walther <90462424+mawalth@users.noreply.github.com>
2021-11-25 13:43:47 +0500

    Add files via upload

---
+++ WP-1.1 (program)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WP_1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = 5; 
+            b = 2; // Входные данные (1 точка)
+
+            Console.Write("Входные данные: ");
+            Console.Write("A = 0, B= 0"); // Пользовательский интерфейс
+            Console.WriteLine();
+            
+            c = (a + 2 * b) * (a - 2 * b); // Выходные данные (2 точка)
+            Console.Write("Выходные данные: " + c); 
+            Console.ReadKey();
+        }
+    }
+}
